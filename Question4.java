54d9233d36d45e1a8475cb0038d8213060d7fc7e
Omari Lyn <lynomari@gmail.com>
2025-02-10 03:13:07 -0500

    Solved Question 4

--- Question4.java
+++ Question4.java
@@ -4,5 +4,21 @@
  * The first few are therefore 1, 1, 1+1=2, 1+2=3, 2+3=5, 3+5=8
  */
 public class Question4 {
-    
+    public static void fibonacci(int n){
+        int a = 1;
+        int b = 1;
+        int temp = 1;
+
+        for(int i = 0; i < n; i++){
+            System.out.println(a);
+            temp = b;
+            b = a+b;
+            a = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        fibonacci(10);
+        
+    }
 }
