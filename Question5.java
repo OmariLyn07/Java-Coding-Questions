25843ca734a5747a26cb74cc8e7b8c8394511c92
Omari Lyn <lynomari@gmail.com>
2025-02-10 03:19:43 -0500

    Solved Question 5

--- Question5.java
+++ Question5.java
@@ -2,5 +2,24 @@
  * So for 2342 it should return [2,3,4,2].
  */
 public class Question5 {
+    static int[] digits(int n){
+        String s = String.valueOf(n);
+        int[] box = new int[s.length()];
+
+        for(int i = 0; i < s.length(); i++){
+            box[i] = Character.getNumericValue(s.charAt(i));
+        }
+
+        return box;
+    }
+
+    public static void main(String[] args) {
+        int[] b = digits(2342);
+
+        for(int i : b){
+            System.out.println(i);
+        }
+        System.out.println(b.toString());
+    }
     
 }
